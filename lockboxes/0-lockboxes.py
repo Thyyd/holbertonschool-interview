bbe3c582c1d0dc79d49c38aa89a1b90e039d44e1
Thyyd <roastingblade@gmail.com>
2026-01-23 14:52:28 +0100

    MàJ fichier 0-lockboxes : Ajout des descriptions docstring pour la fonction et le module

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,4 +1,22 @@
+#!/usr/bin/python3
+"""
+This module contains a function that returns True of False
+if we can open all the boxes
+"""
+
+
 def canUnlockAll(boxes):
+    """
+    Function canUnlockAll that checks if we can unlock every box contained
+    in a bigger one.
+
+    Parameters :
+    boxes : List containing another list composed by entire values
+
+    Return :
+    True if all the boxes are unlocked
+    False if they're not all unlocked
+    """
     keys = [0]
     for index in keys:
         for element in boxes[index]:
