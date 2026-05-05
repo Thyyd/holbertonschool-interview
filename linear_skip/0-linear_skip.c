a926986f5452cd0686686c4cdd728ec1e7d9ef9a
Thyyd <roastingblade@gmail.com>
2026-05-05 12:14:02 +0200

    Correction Betty

--- linear_skip/0-linear_skip.c
+++ linear_skip/0-linear_skip.c
@@ -10,14 +10,11 @@
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
 	skiplist_t *express, *prev;
-
 	if (!list)
 		return (NULL);
-
 	express = list;
 	prev = list;
 
-	/* Step 1: Browse the express lane */
 	while (express->express)
 	{
 		prev = express;
@@ -27,8 +24,6 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 		if (express->n >= value)
 			break;
 	}
-
-	/* Step 2: Determine the search interval */
 	if (!express->express && express->n < value)
 	{
 		/* Value might be after the last express node */
@@ -45,8 +40,6 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 			   prev->index, express->index);
 		express = prev->express;
 	}
-
-	/* Step 3: Linear search in the interval */
 	while (prev && prev->index <= express->index)
 	{
 		printf("Value checked at index [%lu] = [%d]\n", prev->index, prev->n);
@@ -54,6 +47,5 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 			return (prev);
 		prev = prev->next;
 	}
-
 	return (NULL);
 }
